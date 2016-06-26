fd1b50970c5cc773743a24733f1efa2b05015843
IncognitoJam <IncognitoJam@users.noreply.github.com>
2016-06-26 11:43:33 +0100

    Update TrainTicketLoginListener.java

--- src/main/java/me/wizzledonker/plugins/trainticket/TrainTicketLoginListener.java
+++ src/main/java/me/wizzledonker/plugins/trainticket/TrainTicketLoginListener.java
@@ -1,22 +1,23 @@
 package main.java.me.wizzledonker.plugins.trainticket;
- 
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.Listener;
-public class TrainTicketLoginListener
-implements Listener
-{
-public static Trainticket plugin;
- 
-public TrainTicketLoginListener(Trainticket instance)
-{
-plugin = instance;
-}
-@EventHandler
-public void trainLoginListener(PlayerLoginEvent event) {    /*Fixes the error where TrainTickets cease to be TrainTickets if player logs off*/
-	Player player = event.getPlayer();
-	plugin.setTicket(player, true);
-	}    
+
+public class TrainTicketLoginListener implements Listener {
+
+    public static Trainticket plugin;
+
+    public TrainTicketLoginListener(Trainticket instance) {
+        plugin = instance;
+    }
+
+    @EventHandler
+    public void trainLoginListener(PlayerLoginEvent event) {
+        /*Fixes the error where TrainTickets cease to be TrainTickets if player logs off*/
+        Player player = event.getPlayer();
+        plugin.setTicket(player, true);
+    }
+
 }
